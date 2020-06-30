2a027e8bc57e8eb8610599b290ea20ade2e87b76
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-29 21:22:42 -0300

    Adicionando tabela de eventos

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de Criação do Evento')
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
