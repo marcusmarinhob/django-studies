c36a62654fb2a57dac598f00588a35c6ce57df73
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-06-29 21:30:58 -0300

    Adicionando filtros e titulo padrão

--- agenda/core/admin.py
+++ agenda/core/admin.py
@@ -5,5 +5,6 @@ from core.models import Evento
 
 class EventoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo', 'data_evento',)
 
 admin.site.register(Evento, EventoAdmin)
--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Evento(models.Model):
@@ -6,6 +7,7 @@ class Evento(models.Model):
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de Criação do Evento')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'evento'
